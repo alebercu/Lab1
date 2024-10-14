19552d8f0b69c148d250b371f0dc19b436a69a84
alebercu <149473284+alebercu@users.noreply.github.com>
2024-10-14 18:13:26 +0300

    23

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,7 +1,29 @@
 import java.util.Arrays
 
 public class Aufgabe1 {
-    public int[] failedGrades(int[] grades){
+    public int[] failedGrades(int[] grades) {
         return Arrays.stream(grades).filter(grade -> grade <= 40).toArray();
     }
+
+    public double averageGrade(int[] grades) {
+        return (double) Arrays.stream(grades).sum() / grades.length;
+    }
+
+    private int howToRound(int grade) {
+        if (grade > 37) {
+            int diff = 5 - grade % 5;
+            if (diff < 3) {
+                grade += diff;
+            }
+        }
+        return grade;
+    }
+
+    public int[] roundedGrades(int[] grades) {
+        return Arrays.stream(grades).map(this::howToRound).toArray();
+    }
+
+
+
+
 }
