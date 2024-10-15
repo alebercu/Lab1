6334b109ee782df60853540899588ad4021c20c7
alebercu <149473284+alebercu@users.noreply.github.com>
2024-10-15 19:05:43 +0300

    4.3

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -1,10 +1,28 @@
-import java.util.Arrays
+import java.util.Arrays;
+
 
 public class Aufgabe4 {
 
-    public int mostExpensive(int[] prices){
-        return Arrays.stream(prices).max().getAsInt();
+    public int cheapest(int[] prices){
+        return Arrays.stream(prices).min().getAsInt();
     }
 
+    public int mostExpensive(int[] pricesT, int[] pricesU){
+        int maxT = Arrays.stream(pricesT).max().getAsInt();
+        int maxU = Arrays.stream(pricesU).max().getAsInt();
+        return Math.max(maxT, maxU);
+    }
+
+    public int mostExpensiveInBudget(int[] prices, int budget){
+        int maxPrice = -1;
+            for (int price : prices) {
+                if (price <= budget && price > maxPrice) {
+                    maxPrice = price;
+                }
+            }
+            return maxPrice;
+        }
 
 }
+
+
